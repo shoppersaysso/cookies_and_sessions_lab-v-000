ddd76cb1690d512471a7718599731ff80a77ed27
Sara Hopper <sara.hopper@gmail.com>
2017-08-08 16:41:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   helper_method :current_cart
 
   def current_cart
     session[:cart] ||= []
   end
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 end
